0badcb20643330165f19d5c5b24fff7f5f0936db
Chris Clifford and Quinn Schwartz <chris@>
2016-10-17 16:27:18 -0700

    add html, js files with code that moves character in a string

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+var firstLast = function(input) {
+  var sentenceLength = input.length;
+  var firstLetter = input.charAt(0);
+  var lastLetter = input.charAt(sentenceLength - 1 );
+  var firstLast = firstLetter + lastLetter;
+  return firstLast.toUpperCase();
+};
+
+var reverseLetters = function(reverse) {
+  var firstLetter = reverse.charAt(0);
+  var lastLetter = reverse.charAt(1);
+  var reversedLetters = lastLetter + firstLetter;
+  return reversedLetters;
+};
+
+var combineFunctions = function(combine) {
+  var output = firstLast(combine);
+  var output2 = reverseLetters(output);
+  var output3 = combine + output2;
+  return output3;
+};
+
+var fourthFunction = function(input) {
+  var sentenceLength = input.length;
+  var divide = Math.round(sentenceLength / 2);
+  var locateLetter = input.charAt(divide);
+  var combine = locateLetter + combineFunctions(input);
+
+  console.log(combine);
+};
+
+var input = prompt("Enter a sentence:");
+
+fourthFunction(input);
